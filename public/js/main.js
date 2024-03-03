604671a22e8d17fa86d5cae0610b15602ecbb2cf
Hakan Çakmak <hakan.cakmak@live.com>
2024-03-03 23:22:50 +0300

    form and button states was completed.

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,33 @@
 const socket = io()
 
+// Elements
 const messageForm = document.querySelector('#message-form');
 const messageFormInput = document.querySelector('input');
 const messageFormButton = document.querySelector('button');
 const sendLocationButton = document.querySelector('#send-location');
+const messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    })
+    url.insertAdjacentHTML('beforeend', html)  
+})
+
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     messageFormButton.setAttribute('disabled', 'disabled');
@@ -19,7 +37,7 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     socket.emit('sendMessage', message, (error) => {
         messageFormButton.removeAttribute('disabled');
         messageFormInput.value = '';
-        messageFormButton.focus();
+        messageFormInput.focus();
 
         if(error) {
             return console.log(error);
@@ -34,11 +52,14 @@ document.querySelector('#send-location').addEventListener('click', () => {
         return alert('Geolocation is not supported by your browser!');
     }
 
+    sendLocationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            sendLocationButton.removeAttribute('disabled')
             console.log('Location shared!');
         })
     })
