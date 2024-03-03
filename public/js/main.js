2b71830b8eabda18e56dd3abbb9e270a6fd7bcd1
Hakan Çakmak <hakan.cakmak@live.com>
2024-03-03 15:05:50 +0300

    form and button states was added.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 const socket = io()
 
+const messageForm = document.querySelector('#message-form');
+const messageFormInput = document.querySelector('input');
+const messageFormButton = document.querySelector('button');
+const sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
     console.log(message)
 })
@@ -7,9 +12,21 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
+    messageFormButton.setAttribute('disabled', 'disabled');
+
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        messageFormButton.removeAttribute('disabled');
+        messageFormInput.value = '';
+        messageFormButton.focus();
+
+        if(error) {
+            return console.log(error);
+        }
+
+        console.log('Message delivered');
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
